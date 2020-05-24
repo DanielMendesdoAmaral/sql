a58cbc02e29a082b5c76f5266ffa8684b14fc8dd
Daniel Amaral <daniel.amaral720@gmail.com>
2020-05-23 23:06:09 -0300

    Aula 14

--- Curso em Vídeo/Curso em Vídeo/Dumps (Leia o arquivo de texto)/SQLQuery1.sql
+++ Curso em Vídeo/Curso em Vídeo/Dumps (Leia o arquivo de texto)/SQLQuery1.sql
@@ -434,3 +434,39 @@ SELECT carga, COUNT(*) AS quantidade FROM cursos
 WHERE carga > (SELECT AVG(carga) FROM cursos) -- Sleciona todos os cursos com carga maior que a média de carga de todos os cursos.
 GROUP BY carga
 ORDER BY carga;
+
+-- EXERCÍCIOS
+
+USE cadastro;
+SELECT * FROM gafanhotos;
+
+-- 1- Uma lista com as profissões dos gafanhotos e seus respectivos quantitativos.
+
+USE cadastro;
+SELECT profissao, COUNT(*) AS quantidade FROM gafanhotos
+GROUP BY profissao
+ORDER BY quantidade;
+
+-- 2- Quantos gafanhotos e quantas gafanhotas nasceram após 01/Jan/2005?
+
+USE cadastro;
+SELECT sexo, COUNT(*) AS quantidade FROM gafanhotos
+WHERE nascimento > '2005-01-01'
+GROUP BY sexo 
+ORDER BY quantidade;
+
+-- 3- Uma lista com os gafanhotos que nasceram fora do Brasil, mostrando o país de origem e o total de pessoas nascidas lá. Só nos interessam os países que tiverem mais de 3 gafanhotos com essa nacionalidade.
+
+USE cadastro;
+SELECT nacionalidade, COUNT(*) AS quantidade FROM gafanhotos 
+GROUP BY nacionalidade
+HAVING COUNT(*) > 3
+ORDER BY quantidade;
+
+-- 4- Uma lista agrupada pela altura dos gafanhotos, mostrando quantas pessoas pesam mais de 100kg e que estão acima da média de altura de todos os cadastrados.
+
+USE cadastro;
+SELECT altura, COUNT(*) AS quantidade FROM gafanhotos
+WHERE peso > 100 AND altura > (SELECT AVG(altura) FROM gafanhotos)
+GROUP BY altura
+ORDER BY quantidade;
